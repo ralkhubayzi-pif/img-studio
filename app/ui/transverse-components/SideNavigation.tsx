18e4dab5fcf5c538724ed827e197d655ee005082
Razan <ralkhubayzi@pif.gov.sa>
2025-10-02 16:57:22 +0300

    update href

--- app/ui/transverse-components/SideNavigation.tsx
+++ app/ui/transverse-components/SideNavigation.tsx
@@ -149,7 +149,7 @@ export default function SideNav() {
         >
           / Made with <span style={{ margin: 1, color: palette.primary.main }}>❤</span> by
           <a
-            href="mailto:cloud.management@pif.gov.sa"
+            href="https://pif.gov.sa"
             target="_blank"
             rel="noopener noreferrer"
             style={{
